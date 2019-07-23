3c53343efde0c115a1ff3e1a0a8aba07953c0c10
evillt <ijoec123@gmail.com>
2019-07-24 01:49:59 +0800

    style!: tweak
    drop `pageFont` and `codeFont`

--- src/utils/cssVariables.js
+++ src/utils/cssVariables.js
@@ -6,6 +6,7 @@ export default {
   purple: '#F81CE5',
   selection: 'var(--cyan)',
   textColor: '#333',
+  textSize: '14px',
   linkColor: '#333',
   accentColor: 'var(--success-color)',
   borderColor: '#eaeaea',
@@ -14,10 +15,6 @@ export default {
   warningColor: '#F5A623',
   inlineCodeColor: 'var(--purple)',
   gap: '20px',
-  pageFont:
-    '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
-  codeFont:
-    'Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace, serif',
   tableHeaderBackground: '#fafafa',
   tableHeaderColor: '#666'
 }
