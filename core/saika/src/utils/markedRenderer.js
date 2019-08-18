f6289ee46c68f6c691efe70fb5c482ca82e2762a
evillt <ijoec123@gmail.com>
2019-08-18 16:58:03 +0800

    chore: tweak

--- core/saika/src/utils/markedRenderer.js
+++ core/saika/src/utils/markedRenderer.js
@@ -21,8 +21,9 @@ export default function(hooks) {
       ''}" alt="${text}" loading="lazy">`
   }
 
-  renderer.link = (href, _, text) =>
-    `<SaikaLink to="${href}">${text}</SaikaLink>`
+  renderer.link = (href, _, text) => {
+    return `<SaikaLink to="${href}">${text}</SaikaLink>`
+  }
 
   // Disable template interpolation in code
   renderer.codespan = text => `<code v-pre class="inline-code">${text}</code>`
