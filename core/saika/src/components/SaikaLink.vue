2a3e4e5e411b4b6f7a4b9ce1083b35009b293013
evillt <ijoec123@gmail.com>
2019-08-20 21:35:13 +0800

    chore: add `rel` for external link

--- core/saika/src/components/SaikaLink.vue
+++ core/saika/src/components/SaikaLink.vue
@@ -19,7 +19,8 @@ export default {
           attrs: {
             ...attrs,
             href: to,
-            target: '_blank'
+            target: '_blank',
+            rel: 'noopener noreferrer'
           }
         },
         [
