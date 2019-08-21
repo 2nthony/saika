d8981cd638438385f2d8f970cddef2b019b1927b
evillt <ijoec123@gmail.com>
2019-08-21 13:37:39 +0800

    fix($theme-docs[sidebar]): remove trailing HTML

--- themes/docs/src/components/ContentToc.vue
+++ themes/docs/src/components/ContentToc.vue
@@ -37,7 +37,7 @@ export default {
               },
               key: heading.slug
             },
-            heading.text
+            heading.raw.replace(/<.*>\s*$/g, '')
           )
         })
       )
