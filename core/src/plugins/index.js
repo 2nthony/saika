2231da0d27fabf39e0cf895e7cb72b2a3782f8f9
EVILLT <53422750+eviIIt@users.noreply.github.com>
2019-09-30 15:55:45 +0800

    revert($core): drop lazy loading for image (#40)
    * revert($core): drop lazy loading for image
    
    Because in chrome, sometimes flashing
    
    * revert($core): drop lazy loading form `ImageZoom.vue`

--- core/src/plugins/index.js
+++ core/src/plugins/index.js
@@ -4,7 +4,6 @@ import prefetchMarkdown from './prefetch-markdown'
 
 // marked renderer
 import markedRendererHeading from './marked-renderer-heading'
-import markedRendererImage from './marked-renderer-image'
 import markedRendererLink from './marked-renderer-link'
 import markedRendererCode from './marked-renderer-code'
 import markedRendererHoistTags from './marked-renderer-hoist-tags'
@@ -14,7 +13,6 @@ export default [
   bannerAndFooter,
   prefetchMarkdown,
   markedRendererHeading,
-  markedRendererImage,
   markedRendererLink,
   markedRendererCode,
   markedRendererHoistTags
